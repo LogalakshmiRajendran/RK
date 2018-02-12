0dd950c66ed6c3b09e375754f4c78b4d7b35d8a0
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-02-12 16:11:53 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int k;
+printf("enter the string");
+scanf("%s",&a);
+printf("enter the k value");
+scanf("%d",&k);
+for(i=0;a[i]<=k;i++)
+{
+printf("%d",a[i]);
+}
+}
